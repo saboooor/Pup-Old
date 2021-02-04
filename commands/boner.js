0bc735adc78cd9582dca21b9e703ddb8e8034377
Saboor <saboor547@gmail.com>
2021-02-04 13:56:11 -0700

    clean up, optimize instaboner

--- commands/boner.js
+++ commands/boner.js
@@ -9,13 +9,12 @@ module.exports = {
 		let penis = '8D';
 		let randomcolor = '';
 		const ms = 1000;
-		const member = message.member;
 		for (let step = 0; step < random; step++) {
 			randomcolor = Math.round(Math.random() * 16777215);
 			sleep(ms);
 			await pp.edit('', { embed: {
 				color: randomcolor,
-				title: `${member.displayName}'s pp size`,
+				title: `${message.member.displayName}'s pp size`,
 				description: `${penis}`,
 			} });
 			penis = penis.replace('=', '==').replace('8D', '8=D');
@@ -24,7 +23,7 @@ module.exports = {
 		if (sike == 5) {
 			await pp.edit({ embed: {
 				color: randomcolor,
-				title: `${member.displayName}'s pp size`,
+				title: `${message.member.displayName}'s pp size`,
 				description: 'sike bitch',
 				footer: {
 					text: 'u have no pp',
@@ -34,7 +33,7 @@ module.exports = {
 		}
 		await pp.edit({ embed: {
 			color: randomcolor,
-			title: `${member.displayName}'s pp size`,
+			title: `${message.member.displayName}'s pp size`,
 			description: pp.embeds[0].description,
 			footer: {
 				text: `pp size = ${random}"`,
--- commands/instaboner.js
+++ commands/instaboner.js
@@ -7,18 +7,14 @@ module.exports = {
 		const random = Math.round(Math.random() * 35);
 		const pp = await message.channel.send('Calculating pp size..');
 		let penis = '8D';
-		let randomcolor = '';
-		const ms = 1000;
-		const member = message.member;
 		for (let step = 0; step < random; step++) {
-			randomcolor = Math.round(Math.random() * 16777215);
 			penis = penis.replace('=', '==').replace('8D', '8=D');
 		}
 		const sike = Math.round(Math.random() * 10);
 		if (sike == 5) {
 			await pp.edit({ content: '', embed: {
-				color: randomcolor,
-				title: `${member.displayName}'s pp size`,
+				color: Math.round(Math.random() * 16777215),
+				title: `${message.member.displayName}'s pp size`,
 				description: 'sike bitch',
 				footer: {
 					text: 'u have no pp',
@@ -27,8 +23,8 @@ module.exports = {
 			return;
 		}
 		await pp.edit({ content: '', embed: {
-			color: randomcolor,
-			title: `${member.displayName}'s pp size`,
+			color: Math.round(Math.random() * 16777215),
+			title: `${message.member.displayName}'s pp size`,
 			description: penis,
 			footer: {
 				text: `pp size = ${random}"`,
