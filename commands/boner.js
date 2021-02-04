def7e0ce98dc8fb7b24205bccf7ae87bb0648bd1
Saboor <saboor547@gmail.com>
2021-02-04 15:37:42 -0700

    hmm

--- commands/boner.js
+++ commands/boner.js
@@ -5,25 +5,25 @@ module.exports = {
 	guildOnly: false,
 	async execute(message, args, client, sleep, config, Client, Discord) {
 		const random = Math.round(Math.random() * 35);
-		const Embed = new Discord.MessageEmbed().setColor(Math.round(Math.random() * 16777215)).setTitle(`${message.member.displayName}'s pp size`);
-		const pp = await message.channel.send(Embed);
+		const Embed = new Discord.MessageEmbed().setColor(Math.round(Math.random() * 16777215)).setTitle(`${message.member.displayName}'s pp size`).setDescription('Calculating...');
+		const pp = message.channel.send(Embed);
 		let penis = '8D';
 		const randomcolor = Math.round(Math.random() * 16777215);
 		const ms = 1000;
 		for (let step = 0; step < random; step++) {
 			sleep(ms);
 			Embed.setDescription(penis);
-			await pp.edit (Embed);
+			pp.edit (Embed);
 			penis = penis.replace('=', '==').replace('8D', '8=D');
 		}
 		const sike = Math.round(Math.random() * 10);
 		if (sike == 5) {
 			Embed.setDescription('SIKE BITCH').setFooter('u have no pp');
-			await pp.edit(Embed);
+			pp.edit(Embed);
 			return;
 		}
 		Embed.setFooter(`pp size = ${random}"`);
-		await pp.edit(Embed);
+		pp.edit(Embed);
 		return;
 
 	},
--- commands/instaboner.js
+++ commands/instaboner.js
@@ -3,22 +3,16 @@ module.exports = {
 	aliases: ['instapp', 'instapenis', 'instaerect'],
 	cooldown: 10,
 	guildOnly: false,
-	async execute(message, args, client, sleep, config, Client, Discord) {
-		const random = Math.round(Math.random() * 35);
-		let penis = '8D';
-		for (let step = 0; step < random; step++) {
-			penis = penis.replace('=', '==').replace('8D', '8=D');
-		}
+	execute(message, args, client, sleep, config, Client, Discord) {
 		const Embed = new Discord.MessageEmbed().setColor(Math.round(Math.random() * 16777215)).setTitle(`${message.member.displayName}'s pp size`);
-		const sike = Math.round(Math.random() * 10);
-		if (sike == 5) {
+		if (Math.round(Math.random() * 10) == 5) {
 			Embed.setDescription('sike bitch').setFooter('u have no pp');
-			await message.channel.send(Embed);
+			message.channel.send(Embed);
 			return;
 		}
-		Embed.setDescription(penis).setFooter(`pp size = ${random}"`);
-		await message.channel.send(Embed);
+		const random = Math.round(Math.random() * 35);
+		Embed.setDescription('8' + '='.repeat(random - 1) + 'D').setFooter(`pp size = ${random}"`);
+		message.channel.send(Embed);
 		return;
-
 	},
 };
