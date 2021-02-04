6069c073c278b2afa56735616a94dc452b194654
Saboor <saboor547@gmail.com>
2021-02-04 14:07:05 -0700

    MessageEmbed go brr

--- commands/instaboner.js
+++ commands/instaboner.js
@@ -5,31 +5,19 @@ module.exports = {
 	guildOnly: false,
 	async execute(message, args, client, sleep, config, Client, Discord) {
 		const random = Math.round(Math.random() * 35);
-		const pp = await message.channel.send('Calculating pp size..');
 		let penis = '8D';
 		for (let step = 0; step < random; step++) {
 			penis = penis.replace('=', '==').replace('8D', '8=D');
 		}
+		const Embed = new Discord.MessageEmbed().setColor(Math.round(Math.random() * 16777215)).setTitle(`${message.member.displayName}'s pp size`);
 		const sike = Math.round(Math.random() * 10);
 		if (sike == 5) {
-			await pp.edit({ content: '', embed: {
-				color: Math.round(Math.random() * 16777215),
-				title: `${message.member.displayName}'s pp size`,
-				description: 'sike bitch',
-				footer: {
-					text: 'u have no pp',
-				},
-			} });
+			Embed.setDescription('sike bitch').setFooter('u have no pp')
+			await message.channel.send(Embed);
 			return;
 		}
-		await pp.edit({ content: '', embed: {
-			color: Math.round(Math.random() * 16777215),
-			title: `${message.member.displayName}'s pp size`,
-			description: penis,
-			footer: {
-				text: `pp size = ${random}"`,
-			},
-		} });
+		Embed.setDescription(penis).setFooter(`pp size = ${random}"`)
+		await message.channel.send(Embed);
 		return;
 
 	},
