5ae34497530906dc780f1e354b63484bc9b0a837
Saboor <saboor547@gmail.com>
2021-02-04 18:04:34 -0700

    ok last one for now

--- reaction/ndreset.js
+++ reaction/ndreset.js
@@ -5,7 +5,7 @@ module.exports = {
 		if (reaction.emoji.name != '📜') return;
 		const member = message.guild.members.cache.find(m => m.id === user.id);
 		if (!member.roles.cache.has('772251811968188447')) {
-			member.send('You need to link your discord account first!\nTo link your discord, do -link and follow the instructions!');
+			member.send('You need to link your discord account first!\nTo link your discord, follow the instructions at <#766565683210354698>');
 			reaction.users.remove(user.id);
 		}
 	},
