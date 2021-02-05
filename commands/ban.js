a7a0841b9f5f43fc910aa6b579e7f74266801e8c
Saboor <saboor547@gmail.com>
2021-02-04 17:29:26 -0700

    o

--- commands/ban.js
+++ commands/ban.js
@@ -9,7 +9,7 @@ module.exports = {
 		if (!message.mentions.users.first()) return message.reply('ur supposed to use a discord user mention dumbass');
 		const user = message.mentions.users.first();
 		const member = message.guild.members.cache.get(user.id);
-		const Embed = new Discord.MessageEmbed().setColor(Math.round(Math.random() * 16777215)).setTitle(`Banned <@${user.id}> for ${args.join(' ').replace(`${args[0]} `, '')}`);
+		const Embed = new Discord.MessageEmbed().setColor(Math.round(Math.random() * 16777215)).setTitle(`Banned ${user.tag} for ${args.join(' ').replace(`${args[0]} `, '')}`);
 		await user.send(`**You've been banned from ${message.guild.name} for ${args.join(' ').replace(`${args[0]} `, '')}**`);
 		await message.channel.send(Embed);
 		try {
