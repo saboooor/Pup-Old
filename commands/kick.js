2f45a0141f7e911dde06319247fbfab2d521ee89
Saboor <saboor547@gmail.com>
2021-02-04 17:05:59 -0700

    o

--- commands/kick.js
+++ commands/kick.js
@@ -7,10 +7,10 @@ module.exports = {
 	permissions: 'KICK_MEMBERS',
 	execute(message, args, client, sleep, config, Client, Discord) {
 		try {
-			const Embed = new Discord.MessageEmbed().setColor(Math.round(Math.random() * 16777215)).setDescription(`Kicked ${member.tag} for ${args.join(' ').replace(`${args[0]} `, '')}`);
 			const member = message.guild.members.cache.find(u => u.id === args[0].replace('<@!', '').replace('>', ''));
 			const user = client.users.cache.find(u => u.id === args[0].replace('<@!', '').replace('>', ''));
-			member.send(`**You've been kicked from ${message.guild.name} for ${args.join(' ').replace(`${args[0]} `, '')}**`);
+			const Embed = new Discord.MessageEmbed().setColor(Math.round(Math.random() * 16777215)).setDescription(`Kicked ${user.tag} for ${args.join(' ').replace(`${args[0]} `, '')}`);
+			user.send(`**You've been kicked from ${message.guild.name} for ${args.join(' ').replace(`${args[0]} `, '')}**`);
 			message.channel.send(Embed);
 			member.kick();
 		}
