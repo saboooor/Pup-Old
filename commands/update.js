6f199a1a23a6386b1efe7735ce21bd6a95440365
Saboor <saboor547@gmail.com>
2021-02-05 03:05:31 -0700

    forgot https

--- commands/update.js
+++ commands/update.js
@@ -3,10 +3,10 @@ module.exports = {
 	cooldown: 10,
 	guildOnly: true,
     async execute(message, args, client, sleep, config, Client, Discord) {
-        const http = require('http'); // or 'https' for https:// URLs
+        const https = require('https'); // or 'https' for https:// URLs
         const fs = require('fs');
         const file = fs.createWriteStream("purpurclip.jar");
-        const request = http.get("https://ci.pl3x.net/job/Purpur/lastSuccessfulBuild/artifact/final/purpurclip.jar", function(response) {
+        const request = https.get("https://ci.pl3x.net/job/Purpur/lastSuccessfulBuild/artifact/final/purpurclip.jar", function(response) {
             response.pipe(file);
         });
     }
