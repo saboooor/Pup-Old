0da63aba7b5a4232acf471f08202eb26c4a5b313
Saboor <saboor547@gmail.com>
2021-02-05 03:31:11 -0700

    hmmmm

--- commands/update.js
+++ commands/update.js
@@ -2,12 +2,33 @@ module.exports = {
 	name: 'update',
 	cooldown: 10,
 	guildOnly: true,
+	argamnt: 1,
+	usage: '<Server ID>',
     async execute(message, args, client, sleep, config, Client, Discord) {
         const https = require('https'); // or 'https' for https:// URLs
         const fs = require('fs');
         const file = fs.createWriteStream("purpurclip.jar");
+        const reply = await message.channel.send('Downloading purpurclip.jar...');
+        Client.login('https://panel.birdflop.com', config.panelapikey, (logged_in, err) => {
+            if (logged_in == false) return message.reply(`Something went wrong\n${err}`);
+        });
         const request = https.get("https://ci.pl3x.net/job/Purpur/lastSuccessfulBuild/artifact/final/purpurclip.jar", function(response) {
             response.pipe(file);
+            const info = await Client.getServerInfo(id).catch((error) => {console.log(error);});
+            file.on('finish', function() {
+                file.close(cb);
+                reply.edit(`Downloaded! Uploading to server... (${info.attributes.name})`);
+            });
         });
+        fetch(`https://panel.birdflop.com/api/client/servers/${args[0]}/files/upload`, {
+            "method": "GET",
+            "headers": {
+                "Accept": "application/json",
+                "Content-Type": "application/json",
+                "Authorization": `Bearer ${config.panelapikey}`
+            }
+        })
+        .then(response => console.log(response))
+        .catch(err => console.error(err));
     }
 }
