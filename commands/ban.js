ddedb3fb36f60ce2cde559e0b036d4b6a8045c3c
Saboor <saboor547@gmail.com>
2021-02-04 17:26:45 -0700

    catch

--- commands/ban.js
+++ commands/ban.js
@@ -7,11 +7,16 @@ module.exports = {
 	permissions: 'BAN_MEMBERS',
 	async execute(message, args, client, sleep, config, Client, Discord) {
 		if (!message.mentions.users.first()) return message.reply('ur supposed to use a discord user mention dumbass');
-		const member = message.guild.members.cache.find(message.mentions.users.first().id);
-		const user = client.users.cache.find(message.mentions.users.first().id);
-		const Embed = new Discord.MessageEmbed().setColor(Math.round(Math.random() * 16777215)).setDescription(`Kicked ${user.tag} for ${args.join(' ').replace(`${args[0]} `, '')}`);
-		await user.send(`**You've been kicked from ${message.guild.name} for ${args.join(' ').replace(`${args[0]} `, '')}**`);
-		await member.ban();
+		const user = message.mentions.users.first();
+		const member = message.guild.members.cache.get(user.id);
+		const Embed = new Discord.MessageEmbed().setColor(Math.round(Math.random() * 16777215)).setTitle(`Banned ${user.mention} for ${args.join(' ').replace(`${args[0]} `, '')}`);
+		await user.send(`**You've been banned from ${message.guild.name} for ${args.join(' ').replace(`${args[0]} `, '')}**`);
 		await message.channel.send(Embed);
+		try {
+			member.ban();
+		}
+		catch (e) {
+			message.channel.send(e.split('at')[0]);
+		}
 	},
 };
--- commands/kick.js
+++ commands/kick.js
@@ -7,11 +7,16 @@ module.exports = {
 	permissions: 'KICK_MEMBERS',
 	async execute(message, args, client, sleep, config, Client, Discord) {
 		if (!message.mentions.users.first()) return message.reply('ur supposed to use a discord user mention dumbass');
-		const member = message.guild.members.cache.find(message.mentions.users.first().id);
-		const user = client.users.cache.find(message.mentions.users.first().id);
-		const Embed = new Discord.MessageEmbed().setColor(Math.round(Math.random() * 16777215)).setDescription(`Kicked ${user.tag} for ${args.join(' ').replace(`${args[0]} `, '')}`);
+		const user = message.mentions.users.first();
+		const member = message.guild.members.cache.get(user.id);
+		const Embed = new Discord.MessageEmbed().setColor(Math.round(Math.random() * 16777215)).setTitle(`Kicked ${user.mention} for ${args.join(' ').replace(`${args[0]} `, '')}`);
 		await user.send(`**You've been kicked from ${message.guild.name} for ${args.join(' ').replace(`${args[0]} `, '')}**`);
-		await member.kick();
 		await message.channel.send(Embed);
+		try {
+			member.kick();
+		}
+		catch (e) {
+			message.channel.send(e.split('at')[0]);
+		}
 	},
 };
