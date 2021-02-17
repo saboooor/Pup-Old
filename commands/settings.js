176c062ccb64fe074c7c2a051dedb495d4621535
Saboor <saboor547@gmail.com>
2021-02-17 01:41:41 -0700

    Revert "db?"
    This reverts commit bfac2d1f52837960fce6ba84db9d13d725940b2f.

--- commands/settings.js
+++ commands/settings.js
@@ -1,3 +1,4 @@
+const settings = require('../settings.json');
 module.exports = {
 	name: 'settings',
 	guildOnly: true,
@@ -6,10 +7,15 @@ module.exports = {
 	usage: '<Setting> <true/false>',
 	permissions: 'ADMINISTRATOR',
 	async execute(message, args, client, sleep, config, Client, Discord) {
+		if (!settings[message.guild.id]) {
+			settings[message.guild.id] = {
+				'leave': true,
+			};
+		}
 		const Embed = new Discord.MessageEmbed()
 			.setColor(Math.floor(Math.random() * 16777215))
 			.setTitle('Settings')
-			.setDescription(`<:leave:794299854595555349> **Leave messages:** true`);
+			.setDescription(`<:leave:794299854595555349> **Leave messages:** ${settings[message.guild.id].leave}`);
 		message.channel.send(Embed);
 	},
 };
