0be2bad05f4c4fcae204ec80869e96c2aa3780fb
saboooor <saboor547@gmail.com>
2021-02-20 02:52:07 -0700

    update -invite

--- commands/invite.js
+++ commands/invite.js
@@ -5,7 +5,7 @@ module.exports = {
 	async execute(message, args, client, sleep, config, Client, Discord) {
 		const Embed = new Discord.MessageEmbed()
 			.setColor(Math.round(Math.random() * 16777215))
-			.setDescription('[**Invite the bot to your server using this invite link!**](https://discord.com/api/oauth2/authorize?client_id=765287593762881616&permissions=8&scope=bot)\n[**Join the bot\'s discord server!**](https://discord.gg/g7hSukX)');
+			.setDescription('[**Invite the bot to your server using this invite link!**](https://discord.com/api/oauth2/authorize?client_id=765287593762881616&permissions=8&scope=bot)\n[**Join the bot\'s discord server!**](https://discord.gg/Bsefgbaedz)\n[**Also check out Nether Depths!**](https://discord.gg/g7hSukX)');
 		await message.channel.send(Embed);
 	},
 };
