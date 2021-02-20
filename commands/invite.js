a271f6dcc74116dfa68829f94eafaf5bb763a5db
saboooor <saboor547@gmail.com>
2021-02-20 03:00:08 -0700

    remove bold

--- commands/invite.js
+++ commands/invite.js
@@ -7,7 +7,7 @@ module.exports = {
 			.setColor(Math.round(Math.random() * 16777215))
 			.addField('**Add the bot:**', '[Invite the bot to your server using this link!](https://discord.com/api/oauth2/authorize?client_id=765287593762881616&permissions=8&scope=bot)')
 			.addField('**Bot Support:**', '[Join the bot\'s discord server!](https://discord.gg/Bsefgbaedz)')
-			.addField('**Nether Depths:**', '[**Also check out Nether Depths!**](https://discord.gg/g7hSukX)');
+			.addField('**Nether Depths:**', '[Also check out Nether Depths!](https://discord.gg/g7hSukX)');
 		await message.channel.send(Embed);
 	},
 };
