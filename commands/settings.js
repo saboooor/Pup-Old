86681d0c3501c6885b9a6020de4e4a281c90c7f5
Saboor <saboor547@gmail.com>
2021-03-06 07:54:40 -0700

    finalize settings command itself

--- commands/settings.js
+++ commands/settings.js
@@ -6,12 +6,22 @@ module.exports = {
 	permissions: 'ADMINISTRATOR',
 	async execute(message, args, client, sleep, config, Client, Discord) {
 		let srvconfig = Object.keys(client.settings.get(message.guild.id)).map(prop => {
-			return `${prop}  :  ${client.settings.get(message.guild.id)[prop]}`;
-		  });	  
+			return `**${prop}** \`${client.settings.get(message.guild.id)[prop]}\``;
+		  });
+		if (args[1]) {
+			const [prop, ...value] = args;
+			if(!client.settings.has(message.guild.id, prop)) {
+				return message.reply("Invalid setting!");
+			}
+			client.settings.set(message.guild.id, value.join(" "), prop);
+		}
+		srvconfig = Object.keys(client.settings.get(message.guild.id)).map(prop => {
+			return `**${prop}** \`${client.settings.get(message.guild.id)[prop]}\``;
+		});
 		const Embed = new Discord.MessageEmbed()
 			.setColor(Math.floor(Math.random() * 16777215))
 			.setTitle('Settings')
-			.setDescription(`**${srvconfig.join("\n")}**`);
+			.setDescription(`${srvconfig.join("\n")}`);
 		message.channel.send(Embed);
 	},
 };
