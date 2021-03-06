9d3291c892e407ba59d36e658eef7e09c8ebeac0
Saboor <saboor547@gmail.com>
2021-03-06 09:54:14 -0700

    idek what im doing at this point

--- commands/settings.js
+++ commands/settings.js
@@ -6,32 +6,9 @@ module.exports = {
 			return `**${prop}** \`${client.settings.get(message.guild.id)[prop]}\``;
 		});
 		if (args[1]) {
-			if (!message.member.hasPermission('ADMINISTRATOR')) return message.reply('You can\'t do that!');
-			const [prop, ...value] = args;
-			if(!client.settings.has(message.guild.id, prop)) {
-				return message.reply("Invalid setting!");
+			if (!message.member.hasPermission('ADMINISTRATOR')) {
+				if (message.author.id !== '249638347306303499') return message.reply('You can\'t do that!');
 			}
-			client.settings.set(message.guild.id, value.join(" "), prop);
-		}
-		srvconfig = Object.keys(client.settings.get(message.guild.id)).map(prop => {
-			return `**${prop}** \`${client.settings.get(message.guild.id)[prop]}\``;
-		});
-		const Embed = new Discord.MessageEmbed()
-			.setColor(Math.floor(Math.random() * 16777215))
-			.setTitle('Settings')
-			.setDescription(`${srvconfig.join("\n")}`);
-		message.channel.send(Embed);
-	},
-},
-{
-	name: 'opset',
-	guildOnly: true,
-	async execute(message, args, client, sleep, config, Client, Discord) {
-		let srvconfig = Object.keys(client.settings.get(message.guild.id)).map(prop => {
-			return `**${prop}** \`${client.settings.get(message.guild.id)[prop]}\``;
-		});
-		if (args[1]) {
-			if (message.author.id !== '249638347306303499') return message.reply('You can\'t do that!');
 			const [prop, ...value] = args;
 			if(!client.settings.has(message.guild.id, prop)) {
 				return message.reply("Invalid setting!");
