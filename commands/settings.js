ba798200e8de7085da919eb2605665d6a4615a20
Saboor <saboor547@gmail.com>
2021-03-06 22:13:43 -0700

    add settings descriptions

--- commands/settings.js
+++ commands/settings.js
@@ -3,9 +3,6 @@ module.exports = {
 	aliases: ['setting'],
 	guildOnly: true,
 	async execute(message, args, client, sleep, config, Client, Discord) {
-		let srvconfig = Object.keys(client.settings.get(message.guild.id)).map(prop => {
-			return `**${prop}**\n\`${client.settings.get(message.guild.id)[prop]}\`\n${prop}desc`;
-		});
 		if (args[1]) {
 			if (!message.member.hasPermission('ADMINISTRATOR')) {
 				if (message.author.id !== '249638347306303499') return message.reply('You can\'t do that!');
@@ -16,13 +13,19 @@ module.exports = {
 			}
 			client.settings.set(message.guild.id, value.join(" "), prop);
 		}
-		srvconfig = Object.keys(client.settings.get(message.guild.id)).map(prop => {
-			return `**${prop}** \`${client.settings.get(message.guild.id)[prop]}\``;
+		let desc = {
+			prefix: '',
+			slurban: '',
+			simpreaction: '',
+			leavemessage: ''
+		}
+		let srvconfig = Object.keys(client.settings.get(message.guild.id)).map(prop => {
+			return `**${prop}**\n\`${client.settings.get(message.guild.id)[prop]}\`\n${desc[prop]}`;
 		});
 		const Embed = new Discord.MessageEmbed()
 			.setColor(Math.floor(Math.random() * 16777215))
 			.setTitle('Settings')
-			.setDescription(srvconfig.join(''))
+			.setDescription(srvconfig.join('\n'))
 			.addField('Usage', `\`${client.settings.get(message.guild.id).prefix}settings [<Setting> <Value>]\``);
 		message.channel.send(Embed);
 	},
