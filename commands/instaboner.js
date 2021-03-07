ab820a6082b08c48890fb1f9c9e7f36296ad46e8
Saboor <saboor547@gmail.com>
2021-03-07 02:13:20 -0700

    add soft/hard pp

--- commands/instaboner.js
+++ commands/instaboner.js
@@ -5,16 +5,19 @@ module.exports = {
 	guildOnly: false,
 	execute(message, args, client, sleep, config, Client, Discord) {
 		const srvconfig = client.settings.get(message.guild.id);
+		const hard = Math.round(Math.random());
+		let hardtxt = 'soft';
+		if (hard == '1') hardtxt = 'hard';
 		const Embed = new Discord.MessageEmbed()
 			.setColor(Math.round(Math.random() * 16777215))
-			.setTitle(`${message.member.displayName}'s pp size`);
+			.setTitle(`${message.member.displayName}'s ${hardtxt} pp size`);
 		if (Math.round(Math.random() * 10) == 5) {
 			Embed.setDescription('SIKE').setFooter('u have no pp');
 			message.channel.send(Embed);
 			return;
 		}
 		const random = Math.round(Math.random() * srvconfig.maxppsize);
-		Embed.setDescription('8' + '='.repeat(random - 1) + 'D').setFooter(`pp size = ${random}"`);
+		Embed.setDescription('8' + '='.repeat(random - 1) + 'D').setFooter(`${hardtxt} pp size = ${random}"`);
 		message.channel.send(Embed);
 	},
 };
