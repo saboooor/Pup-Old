5142298a9e019d92f850aba861d0c811535b3f2a
Saboor <saboor547@gmail.com>
2021-03-07 02:26:10 -0700

    add some restrictions to settings

--- commands/settings.js
+++ commands/settings.js
@@ -7,6 +7,12 @@ module.exports = {
 			if (!message.member.hasPermission('ADMINISTRATOR')) {
 				if (message.author.id !== '249638347306303499') return message.reply('You can\'t do that!');
 			}
+			if (args[0] == 'maxppsize') {
+				if (args[1] > 75) return message.reply('You can\'t set maxppsize to a number over 75!');
+			}
+			if (['slurban', 'simpreaction', 'adfree', 'listsort'].some(word => args[0].toLowerCase().includes(word))) {
+				if (!['true', 'false'].some(word => args[0].toLowerCase().includes(word))) return message.reply('You can only set this as true or false!')
+			}
 			const [prop, ...value] = args;
 			if(!client.settings.has(message.guild.id, prop)) {
 				return message.reply("Invalid setting!");
@@ -14,14 +20,14 @@ module.exports = {
 			client.settings.set(message.guild.id, value.join(" ").replace(/"/g, ''), prop);
 		}
 		const desc = {
-			prefix: '*The bot\'s prefix*',
+			prefix: '*The bot\'s prefix (You can use double quotes (") to include spaces)*',
 			slurban: '*Bans people who say slurs (true/false)*',
 			simpreaction: '*Reacts with "SIMP" on messages with simpy words (true/false)*',
 			leavemessage: '*Can be either false or the message text itself.\nVariables: {USER MENTION} {USER TAG}*',
 			joinmessage: '*Can be either false or the message text itself.\nVariables: {USER MENTION} {USER TAG}*',
 			adfree: '*Gets rid of all references to other servers (true/false)*',
 			listsort: '*Sorts DiscordSRV playerlist (true/false)*',
-			maxppsize: '*Maximum pp size in pp and instapp commands (true/false)*'
+			maxppsize: '*Maximum pp size in pp and instapp commands*'
 		}
 		const srvconfig = Object.keys(client.settings.get(message.guild.id)).map(prop => {
 			return `**${prop}**\n${desc[prop]}\n\`${client.settings.get(message.guild.id)[prop]}\``;
