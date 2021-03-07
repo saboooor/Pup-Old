34c8a8d23de03cdf1c388d42d9ae1ee063f89eae
Saboor <saboor547@gmail.com>
2021-03-06 23:16:04 -0700

    add trainee

--- response/netherlist.js
+++ response/netherlist.js
@@ -21,6 +21,7 @@ module.exports = {
 		const admin = players.filter(word => word.startsWith('ADMIN')).sort().join(', ').replace(/ADMIN • /g, '').replace(/_/g, '\\_');
 		const mod = players.filter(word => word.startsWith('MOD')).sort().join(', ').replace(/MOD • /g, '').replace(/_/g, '\\_');
 		const helper = players.filter(word => word.startsWith('HELPER')).sort().join(', ').replace(/HELPER • /g, '').replace(/_/g, '\\_');
+		const trainee = players.filter(word => word.startsWith('TRAINEE')).sort().join(', ').replace(/TRAINEE • /g, '').replace(/_/g, '\\_');
 		const mvp = players.filter(word => word.startsWith('MVP')).sort().join(', ').replace(/MVP • /g, '').replace(/_/g, '\\_');
 		const vip = players.filter(word => word.startsWith('VIP')).sort().join(', ').replace(/VIP • /g, '').replace(/_/g, '\\_');
 		const goat = players.filter(word => word.startsWith('GOAT')).sort().join(', ').replace(/GOAT • /g, '').replace(/_/g, '\\_');
@@ -33,8 +34,7 @@ module.exports = {
 		const skeleton = players.filter(word => word.startsWith('SKELETON')).sort().join(', ').replace(/SKELETON • /g, '').replace(/_/g, '\\_');
 		const player = players.filter(word => word.startsWith('PLAYER')).sort().join(', ').replace(/PLAYER • /g, '').replace(/_/g, '\\_');
 		const guest = players.filter(word => word.startsWith('GUEST')).sort().join(', ').replace(/GUEST • /g, '').replace(/_/g, '\\_');
-		const other = players
-			.filter(word => !['OWNER', 'ADMIN', 'MOD', 'HELPER', 'MVP', 'VIP', 'GOAT', 'WARDEN', 'AXOLOTL', 'HOGLIN', 'STRIDER', 'BLAZE', 'ENDERMAN', 'SKELETON', 'PLAYER', 'GUEST'].some(a => word.startsWith(a))).sort().join(', ');
+		const other = players.filter(word => !['OWNER', 'ADMIN', 'MOD', 'HELPER', 'TRAINEE', 'MVP', 'VIP', 'GOAT', 'WARDEN', 'AXOLOTL', 'HOGLIN', 'STRIDER', 'BLAZE', 'ENDERMAN', 'SKELETON', 'PLAYER', 'GUEST'].some(a => word.startsWith(a))).sort().join(', ');
 		const Embed = new Discord.MessageEmbed()
 			.setColor(Math.floor(Math.random() * 16777215))
 			.setTitle(count)
@@ -42,6 +42,7 @@ module.exports = {
 		if (admin) Embed.addField('**Admin**', admin);
 		if (mod) Embed.addField('**Mod**', mod);
 		if (helper) Embed.addField('**Helper**', helper);
+		if (trainee) Embed.addField('**Trainee**', trainee);
 		if (mvp) Embed.addField('**MVP**', mvp);
 		if (vip) Embed.addField('**VIP**', vip);
 		if (goat) Embed.addField('**Goat**', goat);
