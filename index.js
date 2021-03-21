c830bd9761c32e97ea701fe27f257be485bf9228
saboooor <saboor547@gmail.com>
2021-03-20 22:17:02 -0600

    SUBSUBCATEGORIES

--- index.js
+++ index.js
@@ -45,10 +45,13 @@ client.commands = new Discord.Collection();
 const cooldowns = new Discord.Collection();
 const commandFolders = fs.readdirSync('./commands');
 for (const folder of commandFolders) {
-	const commandFiles = fs.readdirSync(`./commands/${folder}`).filter(file => file.endsWith('.js'));
-	for (const file of commandFiles) {
-		const command = require(`./commands/${folder}/${file}`);
-		client.commands.set(command.name, command);
+	const commandFolders2 = fs.readdirSync(`./commands/${folder}`);
+	for (const folder2 of commandFolders2) {
+		const commandFiles = fs.readdirSync(`./commands/${folder}/${folder2}`).filter(file => file.endsWith('.js'));
+		for (const file of commandFiles) {
+			const command = require(`./commands/${folder}/${folder2}/${file}`);
+			client.commands.set(command.name, command);
+		}
 	}
 }
 
