7fc4404f68adda82c257f60ea23321f363a0ec45
Saboor <saboor547@gmail.com>
2021-03-21 23:34:31 -0600

    add -bcast

---
+++ commands/private/sab/bcast.js
@@ -0,0 +1,14 @@
+module.exports = {
+	name: 'bcast',
+	guildOnly: false,
+	args: true,
+	usage: '<Message>',
+	async execute(message, args, client, config, Client, Discord) {
+		if (message.author.id !== '249638347306303499') return message.reply('You can\'t do that!');
+		client.guilds.cache.forEach(guild => {
+			guild.owner.send(`**PUP BROADCAST**
+${args.join(' ')}
+*This message was sent to all owners that have Pup in their guild*`)
+		});		
+	},
+};
