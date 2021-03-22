9ff27cc9b6a6d2801fe769b8a0926ef170ef48a5
Saboor <saboor547@gmail.com>
2021-03-21 23:41:31 -0600

    fix -bcast

--- commands/private/sab/bcast.js
+++ commands/private/sab/bcast.js
@@ -6,9 +6,16 @@ module.exports = {
 	async execute(message, args, client, config, Client, Discord) {
 		if (message.author.id !== '249638347306303499') return message.reply('You can\'t do that!');
 		client.guilds.cache.forEach(guild => {
-			guild.owner.send(`**PUP BROADCAST**
+			try {
+				guild.owner.send(`**PUP BROADCAST**
 ${args.join(' ')}
 *This message was sent to all owners that have Pup in their guild*`)
+			}
+			catch (error) {
+				const rn = new Date();
+				const time = `${minTwoDigits(rn.getHours())}:${minTwoDigits(rn.getMinutes())}:${minTwoDigits(rn.getSeconds())}`;
+				console.error(`[${time} ERROR]: ${error}`);
+			}
 		});		
 	},
 };
