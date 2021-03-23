c368f3708bb6941aebb1967859820c5a1806cbad
Saboor <saboor547@gmail.com>
2021-03-22 20:49:12 -0600

    Update removesetting.js

--- commands/private/sab/removesetting.js
+++ commands/private/sab/removesetting.js
@@ -6,15 +6,13 @@ module.exports = {
 	guildOnly: true,
 	async execute(message, args, client, Client, Discord) {
 		if (message.author.id !== '249638347306303499') return message.reply('You can\'t do that!');
-		if (args[1]) {
-			const prop = args[0];
-			client.guilds.cache.forEach(guild => {
-				client.settings.delete(guild.id, prop);
-				const rn = new Date();
-				const time = `${minTwoDigits(rn.getHours())}:${minTwoDigits(rn.getMinutes())}:${minTwoDigits(rn.getSeconds())}`;
-				console.log(`[${time} INFO]: Removed setting: ${prop}`);
-			});
-		}
+		const prop = args[0];
+		client.guilds.cache.forEach(guild => {
+			client.settings.delete(guild.id, prop);
+			const rn = new Date();
+			const time = `${minTwoDigits(rn.getHours())}:${minTwoDigits(rn.getMinutes())}:${minTwoDigits(rn.getSeconds())}`;
+			console.log(`[${time} INFO]: Removed setting: ${prop}`);
+		});
 		const srvconfig = Object.keys(client.settings.get(message.guild.id)).map(prop => {
 			return `**${prop}** \`${client.settings.get(message.guild.id)[prop]}\``;
 		});
