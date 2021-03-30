21096a49c8cc37cf327e1d1a876b08519ba5769a
saboooor <saboor547@gmail.com>
2021-03-30 17:45:13 -0600

    remove -dm

---
+++ slash/private/sab/dm.js
@@ -0,0 +1,32 @@
+module.exports = {
+	name: 'dm',
+	description: 'DM someone through Pup bot.',
+	guildOnly: true,
+	options: [{
+		type: 6,
+		name: 'user',
+		description: 'User to DM',
+		required: true,
+	},
+	{
+		type: 3,
+		name: 'message',
+		description: 'Message to send',
+		required: true,
+	}],
+	async execute(interaction, args, client, Client, Discord) {
+		if (!client.guilds.cache.get('661736128373719141').members.cache.get(interaction.member.user.id).roles.cache.has('699724468469366844')) return;
+		client.users.cache.get(args[0].value).send(args[1].value);
+		const Embed = new Discord.MessageEmbed()
+			.setColor(Math.round(Math.random() * 16777215))
+			.setDescription(`**Message sent to ${client.users.cache.get(args[0].value)}!**\n**Content:** ${args[1].value}\nTo see the response, see ${client.channels.cache.get('776992487537377311')}`);
+		client.api.interactions(interaction.id, interaction.token).callback.post({
+			data: {
+				type: 4,
+				data: {
+					embeds: [Embed],
+				},
+			},
+		});
+	},
+};
