38e5fa9807e6cc29fee31f133371697f057e27a4
saboooor <saboor547@gmail.com>
2021-03-30 22:44:04 -0600

    add /settings

---
+++ slash/admin/other/settings.js
@@ -0,0 +1,112 @@
+module.exports = {
+	name: 'settings',
+	description: 'Configure the bot',
+	guildOnly: true,
+	cooldown: 1,
+	options: [{
+		type: 3,
+		name: 'setting',
+		description: 'The setting to change',
+		choices: [{
+			name: 'prefix',
+			value: 'prefix',
+		},
+		{
+			name: 'simpreaction',
+			value: 'simpreaction',
+		},
+		{
+			name: 'leavemessage',
+			value: 'leavemessage',
+		},
+		{
+			name: 'joinmessage',
+			value: 'joinmessage',
+		},
+		{
+			name: 'adfree',
+			value: 'adfree',
+		},
+		{
+			name: 'maxppsize',
+			value: 'maxppsize',
+		},
+		{
+			name: 'tickets',
+			value: 'tickets',
+		}],
+	},
+	{
+		type: 3,
+		name: 'value',
+		description: 'The value to set the setting to',
+	}],
+	async execute(interaction, args, client, Client, Discord) {
+		if (args[1]) {
+			if (!client.guilds.cache.get(interaction.guild_id).members.cache.get(interaction.member.user.id).hasPermission('ADMINISTRATOR')) {
+				if (interaction.member.user.id !== '249638347306303499') return;
+			}
+			if (args[0].value == 'maxppsize') {
+				if (args[1].value > 75) {
+					return client.api.interactions(interaction.id, interaction.token).callback.post({
+						data: {
+							type: 4,
+							data: {
+								content: 'You can only set maxppsize as less than 76!',
+							},
+						},
+					});
+				}
+			}
+			if (['simpreaction', 'adfree', 'listsort', 'tickets'].some(word => args[0].value.includes(word))) {
+				if (!['true', 'false'].some(word => args[1].value.includes(word))) {
+					return client.api.interactions(interaction.id, interaction.token).callback.post({
+						data: {
+							type: 4,
+							data: {
+								content: 'You can only set this as true or false!',
+							},
+						},
+					});
+				}
+			}
+			const [prop, ...value] = args;
+			if(!client.settings.has(interaction.guild_id, prop)) {
+				return client.api.interactions(interaction.id, interaction.token).callback.post({
+					data: {
+						type: 4,
+						data: {
+							content: 'Invalid setting!',
+						},
+					},
+				});
+			}
+			client.settings.set(interaction.guild_id, value.join(' ').replace(/"/g, ''), prop);
+		}
+		const desc = {
+			prefix: '*The bot\'s prefix (You can use double quotes (") to include spaces)*',
+			simpreaction: '*Reacts with "SIMP" on messages with simpy words (true/false)*',
+			leavemessage: '*Can be either false or the message text itself.\nVariables: {USER MENTION} {USER TAG}*',
+			joinmessage: '*Can be either false or the message text itself.\nVariables: {USER MENTION} {USER TAG}*',
+			adfree: '*Gets rid of all references to other servers (true/false)*',
+			maxppsize: '*Maximum pp size in pp and instapp commands*',
+			tickets: '*Enables tickets (true/false)*',
+		};
+		const srvconfig = Object.keys(client.settings.get(interaction.guild_id)).map(prop => {
+			return `**${prop}**\n${desc[prop]}\n\`${client.settings.get(interaction.guild_id)[prop]}\``;
+		});
+		const Embed = new Discord.MessageEmbed()
+			.setColor(Math.floor(Math.random() * 16777215))
+			.setTitle('Bot Settings')
+			.setDescription(srvconfig.join('\n'))
+			.addField('Usage', '`/settings [<Setting> <Value>]`');
+		client.api.interactions(interaction.id, interaction.token).callback.post({
+			data: {
+				type: 4,
+				data: {
+					content: [Embed],
+				},
+			},
+		});
+	},
+};
--- slash/user/other/help.js
+++ slash/user/other/help.js
@@ -176,7 +176,7 @@ Walks you through how to setup support tickets in your guild
 4. You're done!`);
 		}
 		else if (arg == 'supportpanel') {
-			if (!client.guilds.cache.get(interaction.guild_id).members.cache.get(interaction.member.user.id).permissions.has('ADMINISTRATOR')) return;
+			if (!client.guilds.cache.get(interaction.guild_id).members.cache.get(interaction.member.user.id).hasPermission('ADMINISTRATOR')) return;
 			Embed.setDescription('Created support panel! You may now delete this message, otherwise it\'ll be deleted in 10 seconds');
 			const Panel = new Discord.MessageEmbed()
 				.setColor(3447003)
