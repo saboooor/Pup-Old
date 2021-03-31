3441f58b3c0b3b8cf2c96e0759ca683b46c6a888
saboooor <saboor547@gmail.com>
2021-03-30 22:20:05 -0600

    fix /clear

--- slash/admin/other/clear.js
+++ slash/admin/other/clear.js
@@ -15,21 +15,19 @@ module.exports = {
 		description: 'The amount of messages to clear',
 	}],
 	async execute(interaction, args, client, Client, Discord) {
-		await client.api.interactions(interaction.id, interaction.token).callback.post({
-			data: {
-				type: 5,
-			},
-		});
 		if (args[0].value > 100) {
-			return client.api.webhooks(client.user.id, interaction.token).messages('@original').patch({
+			client.api.interactions(interaction.id, interaction.token).callback.post({
 				data: {
-					content: 'You can only clear 100 messages at once!',
+					type: 4,
+					data: {
+						content: 'You can only clear less than 100 messages at once!',
+					},
 				},
 			});
 		}
-		await client.channels.cache.get(interaction.channel_id).messages.fetch({ limit: args[0].value }).then(messages => {
-			client.channels.cache.get(interaction.channel_id).bulkDelete(messages);
-		});
+		const messages = await client.channels.cache.get(interaction.channel_id).messages.fetch({ limit: args[0].value });
+		await messages.delete(messages.first());
+		await client.channels.cache.get(interaction.channel_id).bulkDelete(messages);
 		if (client.channels.cache.get(interaction.channel_id).name == 'global') {
 			const consolechannel = client.guilds.cache.get(interaction.guild_id).channels.cache.find(c => c.name.includes('console'));
 			if (!consolechannel) return;
@@ -38,14 +36,17 @@ module.exports = {
 		const rn = new Date();
 		const time = `${minTwoDigits(rn.getHours())}:${minTwoDigits(rn.getMinutes())}:${minTwoDigits(rn.getSeconds())}`;
 		console.log(`[${time} INFO]: Cleared ${args[0].value} messages from #${client.channels.cache.get(interaction.channel_id).name} in ${client.guilds.cache.get(interaction.guild_id).name}`);
-		await client.api.webhooks(client.user.id, interaction.token).messages('@original').patch({
+		await client.api.interactions(interaction.id, interaction.token).callback.post({
 			data: {
-				content: `Cleared ${args[0].value} messages!`,
+				type: 4,
+				data: {
+					content: `Cleared ${args[0].value} messages!`,
+				},
 			},
 		});
+		await sleep(5000);
 		const msg = await client.api.webhooks(client.user.id, interaction.token).messages('@original').patch({ data: {} });
 		const pp = new Discord.Message(client, msg, client.channels.cache.get(msg.channel_id));
-		await sleep(5000);
 		await pp.delete();
 	},
 };
