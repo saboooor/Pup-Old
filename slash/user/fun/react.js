26d153e535293c7e8a4515dcf01888f9168b537b
saboooor <saboor547@gmail.com>
2021-03-30 21:28:23 -0600

    add multiple emoji arguments

--- slash/user/fun/react.js
+++ slash/user/fun/react.js
@@ -5,7 +5,6 @@ module.exports = {
 	name: 'react',
 	description: 'React to a message with pup',
 	cooldown: 1,
-	guildOnly: true,
 	options: [{
 		type: 3,
 		name: 'messageid',
@@ -17,13 +16,154 @@ module.exports = {
 		name: 'emoji',
 		description: 'The Emoji that you want Pup to react with',
 		required: true,
+	},
+	{
+		type: 3,
+		name: 'emoji2',
+		description: 'The second Emoji that you want Pup to react with',
+		required: false,
+	},
+	{
+		type: 3,
+		name: 'emoji3',
+		description: 'The third Emoji that you want Pup to react with',
+		required: false,
+	},
+	{
+		type: 3,
+		name: 'emoji4',
+		description: 'The fourth Emoji that you want Pup to react with',
+		required: false,
+	},
+	{
+		type: 3,
+		name: 'emoji5',
+		description: 'The fifth Emoji that you want Pup to react with',
+		required: false,
+	},
+	{
+		type: 3,
+		name: 'emoji6',
+		description: 'The sixth Emoji that you want Pup to react with',
+		required: false,
+	},
+	{
+		type: 3,
+		name: 'emoji7',
+		description: 'The seventh Emoji that you want Pup to react with',
+		required: false,
+	},
+	{
+		type: 3,
+		name: 'emoji8',
+		description: 'The eighth Emoji that you want Pup to react with',
+		required: false,
+	},
+	{
+		type: 3,
+		name: 'emoji9',
+		description: 'The ninth Emoji that you want Pup to react with',
+		required: false,
+	},
+	{
+		type: 3,
+		name: 'emoji10',
+		description: 'The tenth Emoji that you want Pup to react with',
+		required: false,
+	},
+	{
+		type: 3,
+		name: 'emoji11',
+		description: 'The eleventh Emoji that you want Pup to react with',
+		required: false,
+	},
+	{
+		type: 3,
+		name: 'emoji12',
+		description: 'The twelveth Emoji that you want Pup to react with',
+		required: false,
+	},
+	{
+		type: 3,
+		name: 'emoji13',
+		description: 'The thirteenth Emoji that you want Pup to react with',
+		required: false,
+	},
+	{
+		type: 3,
+		name: 'emoji14',
+		description: 'The fourteenth Emoji that you want Pup to react with',
+		required: false,
+	},
+	{
+		type: 3,
+		name: 'emoji15',
+		description: 'The fifteenth Emoji that you want Pup to react with',
+		required: false,
+	},
+	{
+		type: 3,
+		name: 'emoji16',
+		description: 'The sixteenth Emoji that you want Pup to react with',
+		required: false,
+	},
+	{
+		type: 3,
+		name: 'emoji17',
+		description: 'The seventeenth Emoji that you want Pup to react with',
+		required: false,
+	},
+	{
+		type: 3,
+		name: 'emoji18',
+		description: 'The eighteenth Emoji that you want Pup to react with',
+		required: false,
+	},
+	{
+		type: 3,
+		name: 'emoji19',
+		description: 'The nineteenth Emoji that you want Pup to react with',
+		required: false,
+	},
+	{
+		type: 3,
+		name: 'emoji20',
+		description: 'The twentieth Emoji that you want Pup to react with',
+		required: false,
+	},
+	{
+		type: 3,
+		name: 'emoji21',
+		description: 'The twenty first Emoji that you want Pup to react with',
+		required: false,
+	},
+	{
+		type: 3,
+		name: 'emoji22',
+		description: 'The twenty second Emoji that you want Pup to react with',
+		required: false,
+	},
+	{
+		type: 3,
+		name: 'emoji23',
+		description: 'The twenty third Emoji that you want Pup to react with',
+		required: false,
+	},
+	{
+		type: 3,
+		name: 'emoji24',
+		description: 'The twenty fourth Emoji that you want Pup to react with',
+		required: false,
 	}],
 	usage: '<Message ID> <Emoji>',
 	async execute(interaction, args, client, Client, Discord) {
 		try {
 			const msg = await client.channels.cache.get(interaction.channel_id).messages.fetch({ around: args[0].value, limit: 1 });
 			const fetchedMsg = msg.first();
-			fetchedMsg.react(args[1].value);
+			args.forEach(arg => {
+				if (!arg.name.includes('emoji')) return;
+				fetchedMsg.react(arg.value);
+			});
 		}
 		catch {
 			await client.api.interactions(interaction.id, interaction.token).callback.post({
