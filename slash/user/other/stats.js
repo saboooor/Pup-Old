37fd4428a3c1931b3695a10a2e5593266494ab57
saboooor <saboor547@gmail.com>
2021-03-31 17:06:12 -0600

    fix /stats in dm

--- slash/user/other/stats.js
+++ slash/user/other/stats.js
@@ -72,7 +72,7 @@ module.exports = {
 			serverip = args[0].value;
 		}
 		if (id !== '') {
-			Embed.setThumbnail(client.guilds.cache.get(interaction.guild_id).iconURL());
+			if (interaction.guild_id) Embed.setThumbnail(client.guilds.cache.get(interaction.guild_id).iconURL());
 			Client.login(panel, client.config.panelapikey, (logged_in, err) => {
 				if (logged_in == false) {
 					return client.api.webhooks(client.user.id, interaction.token).messages('@original').patch({
