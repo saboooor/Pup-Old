6fd15dfdf7ecc8e8a94c2c88dea4ae995e15dfe1
Saboor <saboor547@gmail.com>
2021-04-02 16:23:25 -0600

    Update bcast.js

--- commands/private/sab/bcast.js
+++ commands/private/sab/bcast.js
@@ -9,11 +9,12 @@ module.exports = {
 	async execute(message, args, client, Client, Discord) {
 		if (message.author.id !== '249638347306303499') return message.reply('You can\'t do that!');
 		client.guilds.cache.forEach(guild => {
-			if (client.userdata.get(guild.owner.id, "unsubbed") == 'true') return;
-			guild.owner.send(`**PUP BROADCAST**
+			if (client.userdata.get(guild.owner.id, "unsubbed") != 'true') {
+				guild.owner.send(`**PUP BROADCAST**
 ${args.join(' ')}
 *This message was sent to all owners that have Pup in their guild*
 React to this message to unsubscribe to the broadcast`).then(msg => msg.react('❌'));
+			};
 		});
 	},
 };
