b843ac905e943dbbb6150856bdbdbea4f1e0f98f
Saboor <saboor547@gmail.com>
2021-04-02 16:21:42 -0600

    hmm

--- commands/private/sab/bcast.js
+++ commands/private/sab/bcast.js
@@ -10,10 +10,9 @@ module.exports = {
 		if (message.author.id !== '249638347306303499') return message.reply('You can\'t do that!');
 		client.guilds.cache.forEach(guild => {
 			if (client.userdata.get(guild.owner.id, "unsubbed") == 'true') return;
-			if (client.guilds.cache.get('811354612547190794').members.cache.has(guild.owner.id)) return;
 			guild.owner.send(`**PUP BROADCAST**
 ${args.join(' ')}
-*This message was sent to all owners that have Pup in their guild and haven't joined the Discord*
+*This message was sent to all owners that have Pup in their guild*
 React to this message to unsubscribe to the broadcast`).then(msg => msg.react('❌'));
 		});
 	},
