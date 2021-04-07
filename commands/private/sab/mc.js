8e7b17a064790bb15c93c8f04e2063b9015a862c
saboooor <saboor547@gmail.com>
2021-04-06 22:14:53 -0600

    add join arg

--- commands/private/sab/mc.js
+++ commands/private/sab/mc.js
@@ -1,19 +1,20 @@
-function minTwoDigits(n) {
-	return (n < 10 ? '0' : '') + n;
-}
 const mc = require('minecraft-protocol');
 module.exports = {
 	name: 'mc',
 	description: 'Join a minecraft server with Pup',
 	args: true,
-	usage: '<Server IP>',
+	argamt: 2,
+	usage: '<join/leave> <Server IP>',
 	async execute(message, args, client, Client, Discord) {
-		const mcclient = mc.createClient({
-			host: args[0],
-			port: 25565,
-			username: client.config.clientemail,
-			password: client.config.clientpassword,
-			auth: 'mojang',
-		});
+		if (args[0] == 'join') {
+			const mcclient = mc.createClient({
+				host: args[1],
+				port: 25565,
+				username: client.config.clientemail,
+				password: client.config.clientpassword,
+				auth: 'mojang',
+			});
+			message.reply('Joined Minecraft Server!');
+		}
 	},
 };
