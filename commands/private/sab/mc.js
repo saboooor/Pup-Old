094f008f5fc1e2d314f9ff911047a33df8294291
saboooor <saboor547@gmail.com>
2021-04-06 23:05:51 -0600

    switch to mineflayer

--- commands/private/sab/mc.js
+++ commands/private/sab/mc.js
@@ -1,30 +1,30 @@
 function sleep(ms) {
 	return new Promise(res => setTimeout(res, ms));
 }
-const mc = require('minecraft-protocol');
+const mineflayer = require('mineflayer');
 module.exports = {
 	name: 'mc',
 	description: 'Join a minecraft server with Pup',
 	args: true,
 	argamt: 2,
-	usage: '<join/leave> <Server IP>',
+	usage: '<join/chat> <Server IP>',
 	async execute(message, args, client, Client, Discord) {
 		if (message.author.id !== '249638347306303499') return message.reply('You can\'t do that!');
 		if (args[0] == 'join') {
-			client.mc = await mc.createClient({
+			if (client.mc) client.mc.quit();
+			client.mc = mineflayer.createBot({
 				host: args[1],
 				port: 25565,
 				username: client.config.clientemail,
 				password: client.config.clientpassword,
-				auth: 'mojang',
 			});
 			await message.reply('Joined Minecraft Server!');
 			await sleep(5000);
-			await client.mc.write('chat', { message: 'Connected with Pup on Discord' });
+			await client.mc.chat('Connected with Pup on Discord');
 		}
 		if (args[0] == 'chat') {
 			await message.reply('Sent chat!');
-			await client.mc.write('chat', { message: args[1] });
+			await client.mc.chat(args[1]);
 		}
 	},
 };
