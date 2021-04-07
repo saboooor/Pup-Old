239c6b882e4b44ed8f756e9eb70329571825780c
saboooor <saboor547@gmail.com>
2021-04-06 22:27:25 -0600

    add dumb

--- commands/private/sab/mc.js
+++ commands/private/sab/mc.js
@@ -1,3 +1,6 @@
+function sleep(ms) {
+	return new Promise(res => setTimeout(res, ms));
+}
 const mc = require('minecraft-protocol');
 module.exports = {
 	name: 'mc',
@@ -7,22 +10,16 @@ module.exports = {
 	usage: '<join/leave> <Server IP>',
 	async execute(message, args, client, Client, Discord) {
 		if (args[0] == 'join') {
-			const mcclient = mc.createClient({
+			const mcclient = await mc.createClient({
 				host: args[1],
 				port: 25565,
 				username: client.config.clientemail,
 				password: client.config.clientpassword,
 				auth: 'mojang',
 			});
-			message.reply('Joined Minecraft Server!');
+			await message.reply('Joined Minecraft Server!');
+			await sleep(5000);
+			await mcclient.write('chat', { message: 'join play.goldenwaters.org now' });
 		}
-		const msg = {
-			translate: 'chat.type.announcement',
-			'with': [
-				'Server',
-				'Hello, world!',
-			],
-		};
-		client.write('chat', { message: JSON.stringify(msg), position: 0, sender: '0' });
 	},
 };
