26ed652a03d43f58dad603f7dcf5ab4655da5461
saboooor <saboor547@gmail.com>
2021-04-07 20:26:34 -0600

    5

--- commands/private/sab/mc.js
+++ commands/private/sab/mc.js
@@ -44,12 +44,12 @@ module.exports = {
 				}
 				if (chatmsg.startsWith('[+]')) {
 					const msgs = ['wb', `wb ${chatmsg.split('] ')[2]}`, `we meet again, ${chatmsg.split('] ')[2]}`, `hello again, ${chatmsg.split('] ')[2]}`, `hi ${chatmsg.split('] ')[2]} :)`, 'WB!!'];
-					const random = Math.round(Math.random() * 6);
+					const random = Math.round(Math.random() * 5);
 					client.mc.chat(msgs[random]);
 				}
 				if (chatmsg.startsWith('[-]')) {
 					const msgs = ['bruh why u leave :(', `k bye ${chatmsg.split('] ')[2]} smh`, `cya ${chatmsg.split('] ')[2]} o/`, `byebye ${chatmsg.split('] ')[2]}`, `join back ${chatmsg.split('] ')[2]} i miss u :(`, 'oh ok bye'];
-					const random = Math.round(Math.random() * 6);
+					const random = Math.round(Math.random() * 5);
 					client.mc.chat(msgs[random]);
 				}
 				if (chatmsg.startsWith('Welcome')) client.mc.chat('Welcome!');
