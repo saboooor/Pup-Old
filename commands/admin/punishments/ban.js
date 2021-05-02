feef264d0773463b5fee106fa4aa9037c75e4433
saboooor <saboor547@gmail.com>
2021-05-02 13:22:50 -0600

    don't require reason for punishments

--- commands/admin/punishments/ban.js
+++ commands/admin/punishments/ban.js
@@ -16,11 +16,19 @@ module.exports = {
 		const author = message.guild.members.cache.get(message.author.id);
 		if (member.roles.highest.rawPosition > author.roles.highest.rawPosition) return message.reply('You can\'t do that! Your role is lower than the user\'s role!');
 		const Embed = new Discord.MessageEmbed()
-			.setColor(Math.round(Math.random() * 16777215))
-			.setTitle(`Banned ${user.tag} for ${args.join(' ').replace(`${args[0]} `, '')}`);
-		await user.send(`**You've been banned from ${message.guild.name} for ${args.join(' ').replace(`${args[0]} `, '')}**`).catch(e => {
-			message.channel.send('Could not DM user! You may have to manually let them know that they have been banned.');
-		});
+			.setColor(Math.round(Math.random() * 16777215));
+		if (args[1]) {
+			Embed.setTitle(`Banned ${user.tag} for ${args.join(' ').replace(`${args[0]} `, '')}`);
+			await user.send(`**You've been banned from ${message.guild.name} for ${args.join(' ').replace(`${args[0]} `, '')}**`).catch(e => {
+				message.channel.send('Could not DM user! You may have to manually let them know that they have been banned.');
+			});
+		}
+		else {
+			Embed.setTitle(`Banned ${user.tag}.`);
+			await user.send(`**You've been banned from ${message.guild.name}.**`).catch(e => {
+				message.channel.send('Could not DM user! You may have to manually let them know that they have been banned.');
+			});
+		}
 		await message.channel.send(Embed);
 		await member.ban().catch(e => message.channel.send(`\`${`${e}`.split('at')[0]}\``));
 		const rn = new Date();
--- commands/admin/punishments/kick.js
+++ commands/admin/punishments/kick.js
@@ -5,8 +5,7 @@ module.exports = {
 	name: 'kick',
 	description: 'Kick someone with a reason',
 	args: true,
-	argamnt: 2,
-	usage: '<User Mention> <Reason>',
+	usage: '<User Mention> [Reason]',
 	permissions: 'KICK_MEMBERS',
 	cooldown: 5,
 	async execute(message, args, client, Client, Discord) {
@@ -16,11 +15,19 @@ module.exports = {
 		const author = message.guild.members.cache.get(message.author.id);
 		if (member.roles.highest.rawPosition > author.roles.highest.rawPosition) return message.reply('You can\'t do that! Your role is lower than the user\'s role!');
 		const Embed = new Discord.MessageEmbed()
-			.setColor(Math.round(Math.random() * 16777215))
-			.setTitle(`Kicked ${user.tag} for ${args.join(' ').replace(`${args[0]} `, '')}`);
-		await user.send(`**You've been kicked from ${message.guild.name} for ${args.join(' ').replace(`${args[0]} `, '')}**`).catch(e => {
-			message.channel.send('Could not DM user! You may have to manually let them know that they have been kicked.');
-		});
+			.setColor(Math.round(Math.random() * 16777215));
+		if (args[1]) {
+			Embed.setTitle(`Kicked ${user.tag} for ${args.join(' ').replace(`${args[0]} `, '')}`);
+			await user.send(`**You've been kicked from ${message.guild.name} for ${args.join(' ').replace(`${args[0]} `, '')}**`).catch(e => {
+				message.channel.send('Could not DM user! You may have to manually let them know that they have been kicked.');
+			});
+		}
+		else {
+			Embed.setTitle(`Kicked ${user.tag}.`);
+			await user.send(`**You've been kicked from ${message.guild.name}.**`).catch(e => {
+				message.channel.send('Could not DM user! You may have to manually let them know that they have been kicked.');
+			});
+		}
 		await message.channel.send(Embed);
 		await member.kick().catch(e => message.channel.send(`\`${`${e}`.split('at')[0]}\``));
 		const rn = new Date();
