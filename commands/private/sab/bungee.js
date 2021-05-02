a090c81d1dd79276e9956ff62c347325de702daf
saboooor <saboor547@gmail.com>
2021-05-02 15:24:17 -0600

    change required role

--- commands/private/sab/bungee.js
+++ commands/private/sab/bungee.js
@@ -9,7 +9,7 @@ module.exports = {
 	usage: '<Command>',
 	async execute(message, args, client, Client, Discord) {
 		if (!client.guilds.cache.get('837116518730694678').members.cache.get(message.member.id)) return message.reply('You can\'t do that!');
-		if (!client.guilds.cache.get('837116518730694678').members.cache.get(message.member.id).roles.cache.has('837117859678781451')) return message.reply('You can\'t do that!');
+		if (!client.guilds.cache.get('837116518730694678').members.cache.get(message.member.id).roles.cache.has('837119752232632380')) return message.reply('You can\'t do that!');
 		Client.login('https://panel.birdflop.com', client.config.panelapikey, (logged_in, err) => {
 			if (logged_in == false) return message.reply(`Something went wrong, please use https://panel.birdflop.com\n${err}`);
 		});
