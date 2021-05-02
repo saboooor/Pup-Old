a04b588d90087cfaec6029d09dec25ec815362dc
saboooor <saboor547@gmail.com>
2021-05-02 16:34:06 -0600

    fix typo

--- commands/private/sab/bungee.js
+++ commands/private/sab/bungee.js
@@ -18,6 +18,6 @@ module.exports = {
 		Client.sendCommand('68505ddb', args.join(' ')).catch((error) => {
 			console.error(`[${time} ERROR]: ${error}`);
 		});
-		message.reply(`Sent command \`${args.join(' ')}}\` to Bungee`);
+		message.reply(`Sent command \`${args.join(' ')}\` to Bungee`);
 	},
 };
