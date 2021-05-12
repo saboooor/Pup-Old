7ad85ef93989055c11ac2b6ce79da9633c53a617
saboooor <saboor547@gmail.com>
2021-05-11 18:08:18 -0600

    update list sorting to match nd's new rank format

--- response/list.js
+++ response/list.js
@@ -61,27 +61,26 @@ module.exports = {
 			if (other) Embed.addField('**Other**', other);
 		}
 		else if (message.guild.id == '661736128373719141') {
-			const owner = players.filter(word => word.startsWith('OWNER')).sort().join(', ').replace(/OWNER • /g, '').replace(/_/g, '\\_');
-			const admin = players.filter(word => word.startsWith('ADMIN')).sort().join(', ').replace(/ADMIN • /g, '').replace(/_/g, '\\_');
-			const mod = players.filter(word => word.startsWith('MOD')).sort().join(', ').replace(/MOD • /g, '').replace(/_/g, '\\_');
-			const helper = players.filter(word => word.startsWith('HELPER')).sort().join(', ').replace(/HELPER • /g, '').replace(/_/g, '\\_');
-			const trainee = players.filter(word => word.startsWith('TRAINEE')).sort().join(', ').replace(/TRAINEE • /g, '').replace(/_/g, '\\_');
+			const owner = players.filter(word => word.startsWith('Founder')).sort().join(', ').replace(/Founder • /g, '').replace(/_/g, '\\_');
+			const admin = players.filter(word => word.startsWith('Administrator')).sort().join(', ').replace(/Administrator • /g, '').replace(/_/g, '\\_');
+			const mod = players.filter(word => word.startsWith('Moderator')).sort().join(', ').replace(/Moderator • /g, '').replace(/_/g, '\\_');
+			const helper = players.filter(word => word.startsWith('Helper')).sort().join(', ').replace(/Helper • /g, '').replace(/_/g, '\\_');
+			const trainee = players.filter(word => word.startsWith('Trainee')).sort().join(', ').replace(/Trainee • /g, '').replace(/_/g, '\\_');
 			const mvp = players.filter(word => word.startsWith('MVP')).sort().join(', ').replace(/MVP • /g, '').replace(/_/g, '\\_');
 			const vip = players.filter(word => word.startsWith('VIP')).sort().join(', ').replace(/VIP • /g, '').replace(/_/g, '\\_');
-			const glowsquid = players.filter(word => word.startsWith('GLOW SQUID')).sort().join(', ').replace(/GLOW SQUID • /g, '').replace(/_/g, '\\_');
-			const warden = players.filter(word => word.startsWith('WARDEN')).sort().join(', ').replace(/WARDEN • /g, '').replace(/_/g, '\\_');
-			const axolotl = players.filter(word => word.startsWith('AXOLOTL')).sort().join(', ').replace(/AXOLOTL • /g, '').replace(/_/g, '\\_');
-			const hoglin = players.filter(word => word.startsWith('HOGLIN')).sort().join(', ').replace(/HOGLIN • /g, '').replace(/_/g, '\\_');
-			const strider = players.filter(word => word.startsWith('STRIDER')).sort().join(', ').replace(/STRIDER • /g, '').replace(/_/g, '\\_');
-			const blaze = players.filter(word => word.startsWith('BLAZE')).sort().join(', ').replace(/BLAZE • /g, '').replace(/_/g, '\\_');
-			const enderman = players.filter(word => word.startsWith('ENDERMAN')).sort().join(', ').replace(/ENDERMAN • /g, '').replace(/_/g, '\\_');
-			const skeleton = players.filter(word => word.startsWith('SKELETON')).sort().join(', ').replace(/SKELETON • /g, '').replace(/_/g, '\\_');
-			const player = players.filter(word => word.startsWith('PLAYER')).sort().join(', ').replace(/PLAYER • /g, '').replace(/_/g, '\\_');
-			const guest = players.filter(word => word.startsWith('GUEST')).sort().join(', ').replace(/GUEST • /g, '').replace(/_/g, '\\_');
-			const other = players.filter(word => !['OWNER', 'ADMIN', 'MOD', 'HELPER', 'TRAINEE', 'MVP', 'VIP', 'GLOW SQUID', 'WARDEN', 'AXOLOTL', 'HOGLIN', 'STRIDER', 'BLAZE', 'ENDERMAN', 'SKELETON', 'PLAYER', 'GUEST'].some(a => word.startsWith(a))).sort().join(', ');
-			if (owner) Embed.addField('**Owner**', owner);
-			if (admin) Embed.addField('**Admin**', admin);
-			if (mod) Embed.addField('**Mod**', mod);
+			const glowsquid = players.filter(word => word.startsWith('Glow Squid')).sort().join(', ').replace(/Glow Squid • /g, '').replace(/_/g, '\\_');
+			const warden = players.filter(word => word.startsWith('Warden')).sort().join(', ').replace(/Warden • /g, '').replace(/_/g, '\\_');
+			const axolotl = players.filter(word => word.startsWith('Axolotl')).sort().join(', ').replace(/Axolotl • /g, '').replace(/_/g, '\\_');
+			const hoglin = players.filter(word => word.startsWith('Hoglin')).sort().join(', ').replace(/Hoglin • /g, '').replace(/_/g, '\\_');
+			const strider = players.filter(word => word.startsWith('Strider')).sort().join(', ').replace(/Strider • /g, '').replace(/_/g, '\\_');
+			const blaze = players.filter(word => word.startsWith('Blaze')).sort().join(', ').replace(/Blaze • /g, '').replace(/_/g, '\\_');
+			const enderman = players.filter(word => word.startsWith('Enderman')).sort().join(', ').replace(/Enderman • /g, '').replace(/_/g, '\\_');
+			const skeleton = players.filter(word => word.startsWith('Skeleton')).sort().join(', ').replace(/Skeleton • /g, '').replace(/_/g, '\\_');
+			const player = players.filter(word => word.startsWith('Player')).sort().join(', ').replace(/Player • /g, '').replace(/_/g, '\\_');
+			const other = players.filter(word => !['Founder', 'Administrator', 'Moderator', 'Helper', 'Trainee', 'MVP', 'VIP', 'Glow Squid', 'Warden', 'Axolotl', 'Hoglin', 'Strider', 'Blaze', 'Enderman', 'Skeleton', 'Player'].some(a => word.startsWith(a))).sort().join(', ');
+			if (owner) Embed.addField('**Founder**', owner);
+			if (admin) Embed.addField('**Administrator**', admin);
+			if (mod) Embed.addField('**Moderator**', mod);
 			if (helper) Embed.addField('**Helper**', helper);
 			if (trainee) Embed.addField('**Trainee**', trainee);
 			if (mvp) Embed.addField('**MVP**', mvp);
@@ -95,7 +94,6 @@ module.exports = {
 			if (enderman) Embed.addField('**Enderman**', enderman);
 			if (skeleton) Embed.addField('**Skeleton**', skeleton);
 			if (player) Embed.addField('**Player**', player);
-			if (guest) Embed.addField('**Guest**', guest);
 			if (other) Embed.addField('**Other**', other);
 		}
 		const msg = await message.channel.send(Embed);
