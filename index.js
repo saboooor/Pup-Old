a866fc731c25b2fa63756a5547d82a81e4706f88
saboooor <saboor547@gmail.com>
2021-05-13 19:46:07 -0600

    get rid of users cuz too much

--- index.js
+++ index.js
@@ -10,7 +10,6 @@ const cron = require('node-cron');
 const client = new Discord.Client({ partials: ['MESSAGE', 'CHANNEL', 'REACTION'], intents: ['GUILDS', 'GUILD_MESSAGES', 'GUILD_MEMBERS', 'GUILD_BANS', 'GUILD_PRESENCES', 'GUILD_MESSAGE_REACTIONS', 'DIRECT_MESSAGES', 'DIRECT_MESSAGE_REACTIONS'], allowedMentions: { parse: ['users', 'roles', 'everyone'], repliedUser: true } });
 client.config = require('./config.json');
 const guildnames = [];
-const usernames = [];
 function minTwoDigits(n) {
 	return (n < 10 ? '0' : '') + n;
 }
@@ -37,7 +36,7 @@ for (const folder of slashcommandFolders) {
 }
 
 client.once('ready', () => {
-	client.user.setPresence({ activities: [{ name: 'with you ;)', type: 'PLAYING' }], status: 'dnd' });
+	client.user.setPresence({ activities: [{ name: 'Just Restarted!', type: 'PLAYING' }], status: 'dnd' });
 	client.channels.cache.get('812082273393704960').messages.fetch({ limit: 1 }).then(msg => {
 		const mesg = msg.first();
 		if (mesg.content !== 'Started Successfully!') client.channels.cache.get('812082273393704960').send('Started Successfully!');
@@ -45,15 +44,12 @@ client.once('ready', () => {
 	client.guilds.cache.forEach(guild => {
 		guildnames.push(guild.name);
 	});
-	client.users.cache.forEach(user => {
-		usernames.push(user.username);
-	});
 	client.slashcommands.forEach(async command => {
 		const commands = await client.api.applications(client.user.id).commands.get();
 		if (commands.find(c => c.name == command.name) && commands.find(c => c.description == command.description)) return;
 		const rn = new Date();
 		const time = `${minTwoDigits(rn.getHours())}:${minTwoDigits(rn.getMinutes())}:${minTwoDigits(rn.getSeconds())}`;
-		await console.log(`[${time} INFO]: Detected ${command.name} has some changes! Updating command...`);
+		console.log(`[${time} INFO]: Detected ${command.name} has some changes! Updating command...`);
 		await client.api.applications(client.user.id).commands.post({
 			data: {
 				name: command.name,
@@ -324,17 +320,12 @@ setInterval(async () => {
 		['WATCHING', `${client.channels.cache.size} Channels`],
 		['COMPETING', `${client.guilds.cache.size} Servers`],
 		['PLAYING', '{GUILD}'],
-		['PLAYING', '{USER}'],
 	];
 	const activitynumber = Math.round(Math.random() * (activities.length - 1));
 	const activity = activities[activitynumber];
 	if (activity[1] == '{GUILD}') {
 		const activityguildnumber = Math.round(Math.random() * (guildnames.length - 1));
 		activity[1] = `in ${guildnames[activityguildnumber]}`;
-	}
-	if (activity[1] == '{USER}') {
-		const activityusernumber = Math.round(Math.random() * (usernames.length - 1));
-		activity[1] = `with ${usernames[activityusernumber]}`;
 	}
 	const duration = moment.duration(client.uptime).format('D [days], H [hrs], m [mins], s [secs]');
 	if (activity[1] == '{UPTIME}') activity[1] = `for ${duration}`;
