e16a545f3c0ff703d30909b7a666d50721528b1a
saboooor <saboor547@gmail.com>
2021-05-17 21:39:34 -0600

    get rid of dm check when u cant even do it in dms

--- commands/user/other/server.js
+++ commands/user/other/server.js
@@ -4,7 +4,13 @@ module.exports = {
 	aliases: ['inv'],
 	cooldown: 10,
 	async execute(message, args, client, Client, Discord) {
-		const srvconfig = client.settings.get(message.guild.id);
+		let srvconfig = [];
+		if (message.channel.type == 'dm') {
+			srvconfig.adfree = 'false';
+		}
+		else {
+			srvconfig = client.settings.get(message.guild.id);
+		}
 		const Embed = new Discord.MessageEmbed()
 			.setColor(Math.round(Math.random() * 16777215))
 			.addField('**Add the bot:**', '[Invite the bot to your server using this link!](https://discord.com/api/oauth2/authorize?client_id=765287593762881616&permissions=2184703062&scope=bot%20applications.commands)')
