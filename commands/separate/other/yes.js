9291494dae33b792b340c82f009d782fb137e016
saboooor <saboor547@gmail.com>
2021-05-28 19:48:19 -0600

    yes

---
+++ commands/separate/other/yes.js
@@ -0,0 +1,13 @@
+const moment = require('moment');
+module.exports = {
+	name: 'time',
+	description: 'Get time',
+	cooldown: 10,
+	async execute(message, args, client, Client, Discord) {
+		if (message.author.id != '249638347306303499') return;
+		const msg = await message.channel.send(`${moment(Date.now())}`);
+		setInterval(async () => {
+			msg.edit(`${moment(Date.now())}`);
+		}, 1000);
+	},
+};
