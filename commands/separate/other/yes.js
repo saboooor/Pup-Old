33766c5a3b3c54aadca175b28cbfd9b7c6c062fa
saboooor <saboor547@gmail.com>
2021-05-28 20:13:17 -0600

    yes

--- commands/separate/other/yes.js
+++ commands/separate/other/yes.js
@@ -5,9 +5,9 @@ module.exports = {
 	cooldown: 10,
 	async execute(message, args, client, Client, Discord) {
 		if (message.author.id != '249638347306303499') return;
-		const msg = await message.channel.send(`${moment(Date.now())}`);
+		const msg = await message.channel.send('i like dick');
 		setInterval(async () => {
-			msg.edit(`${moment(Date.now())}`);
+			msg.edit(`${new Date(Date.now()).toLocaleString('default', { weekday: 'long', month: 'long', day: 'numeric', year: 'numeric', hour: 'numeric', minute: 'numeric', second: 'numeric', hour12: true })}`);
 		}, 1200);
 	},
 };
